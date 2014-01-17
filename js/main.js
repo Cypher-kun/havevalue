657f52d2bd63b2b0b4ca73dad3c839fdfff7b8da
Anton Shchukin <cypher76@gmail.com>
2014-01-17 21:40:43 +0700

    Marked up profile page

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@
 $(document).on('ready', function(){
     $('.no-placeholder input[placeholder], .no-placeholder textarea[placeholder]').placeholder();
 });
+
+$(document).on('ready', function(){
+    $('.profile__tabs__item__link').on('click', function(event){
+        event.preventDefault();
+
+        if ( ! $(this).hasClass('profile__tabs__item__link_active') ) {
+            $('.profile__tabs__item__link_active').removeClass('profile__tabs__item__link_active');
+            $(this).addClass('profile__tabs__item__link_active');
+
+            $('.profile__pages__item_active').fadeOut();
+            $('.profile__pages__item_active').removeClass('profile__pages__item_active');
+            $( $(this).data('page') ).addClass('profile__pages__item_active');
+            $( $(this).data('page') ).fadeIn();
+        }
+
+    });
+});
